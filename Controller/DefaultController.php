9763558ca36ef23d713b5063bee353b333ab1073
Zanshee <info@zanshee.kiev.ua>
2015-11-21 00:03:09 +0200

    Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zanshee\TelegramBDKBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ZansheeTelegramBDKBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ ZansheeTelegramBDKBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zanshee\TelegramBDKBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZansheeTelegramBDKBundle extends Bundle
+{
+}
